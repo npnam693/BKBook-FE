6ef6dd8bf1ca567d7858ddc244e6d5c1b10fd6de
Nguyen Phi Nam <nguyenphinam2k2@gmail.com>
2023-12-10 19:10:10 +0700

    update firestorage

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,9 +4,9 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: "AIzaSyALc3NGEJuSNjZ0rrd6lSgXgRMt_-0oYEs",
-    authDomain: "bkbook-upload.firebaseapp.com",
-    projectId: "bkbook-upload",
-    storageBucket: "bkbook-upload.appspot.com",
+    authDomain: "bkmotel-upload.firebaseapp.com",
+    projectId: "bkmotel-upload",
+    storageBucket: "bkmotel-upload.appspot.com",
     messagingSenderId: "255503113968",
     appId: "1:255503113968:web:3552a73ca3db3924b92c35",
     measurementId: "G-9X5HJD4DC8"
